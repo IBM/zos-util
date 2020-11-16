0191408f1253b8cb42bbdeed0d1333ab672b4a91
Steven Pitman <pitman@ca.ibm.com>
2020-11-16 20:24:27 +0000

    Improve documentation

--- README.md
+++ README.md
@@ -1,6 +1,34 @@
 # zos-util
 This module provides a Python interface into various z/OS utilities
 
+# API
+See [here](https://www.ibm.com/support/knowledgecenter/SSCH7P_3.8.0/zos_util.html) for the API
+
+# Example
+```python
+import zos_util
+import tempfile
+f = tempfile.NamedTemporaryFile()
+# To specify a file with IBM-1047 code set
+fpath = f.name
+zos_util.chtag(fpath, 1047)
+ 
+# To specify a file with ISO8859-1 code set
+zos_util.chtag(fpath)
+tag_info = zos_util.get_tag_info(fpath)
+print(f"CCSID:{tag_info[0]}, TXT_FLAG:{tag_info[1]}")
+ 
+# set to tag_mixed mode
+zos_util.tag_mixed(fpath)
+tag_info = zos_util.get_tag_info(fpath)
+print(f"CCSID:{tag_info[0]}, TXT_FLAG:{tag_info[1]}")
+ 
+# remove the tag from the file
+zos_util.untag(fpath)
+tag_info = zos_util.get_tag_info(fpath)
+print(f"CCSID:{tag_info[0]}, TXT_FLAG:{tag_info[1]}")
+```
+
 # Build Instruction  
   `python3 ./setup install`
 
--- setup.py
+++ setup.py
@@ -7,14 +7,22 @@ from distutils.core import setup, Extension
 import os
 import sysconfig
 
-
 def main():
     dir_path = os.path.dirname(os.path.realpath(__file__))
     zos_util_ext = Extension("zos_util", sources=["zos_util.c"])
+    with open(os.path.join(dir_path, "README.md")) as f:
+        project_description = f.read()
+
     setup(name="zos_util",
           version="1.0.1",
           license="License :: OSI Approved :: Apache Software License",
           description="This module provides a Python interface into various z/OS utilities",
+          long_description=project_description,
+          long_description_content_type='text/markdown',
+          project_urls={
+            "Documentation": "https://www.ibm.com/support/knowledgecenter/SSCH7P_3.8.0/zos_util.html",
+            "Source Code": "https://github.com/IBM/zos-util",
+          },
           author="IBM",
           ext_modules=[zos_util_ext]
           )
